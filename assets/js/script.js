cbc34c13bbf1250fa1e0a4bc0dc8deb2f9ffa195
Mona Lisa <Mona Lisa>
2019-06-29 23:16:58 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    (function () {
+        'use strict';
+        window.addEventListener('load', function () {
+            var forms = document.getElementsByClassName('needs-validation');
+            var validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+
+
+
+
+});
+var password = document.getElementById("password")
+    , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("كلمه السر غير متطابقة");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
